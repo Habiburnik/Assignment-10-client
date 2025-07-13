bda6e96c327d92ea35399f0dd241706fe191a65c
Md. Habibur Rahman <rhabibur@ghit.com>
2025-07-13 14:06:38 +0600

    final commit2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import userIcon from '../assets/user.png'
 import { AuthContext } from '../provider/AuthProvider';
 import { FiLogIn } from "react-icons/fi";
 import { FiLogOut } from "react-icons/fi";
+import icon from '../../public/vite.svg'
 
 const Navbar = () => {
     const { user, logOut } = useContext(AuthContext)
